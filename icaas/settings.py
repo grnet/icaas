897e3193aba00418dcb72d030c0b4a2d36d0bb26
Nikos Skalkotos <skalkoto@grnet.gr>
2015-07-16 16:41:00 +0300

    Unpersonalize the settings file

--- icaas/settings.py
+++ icaas/settings.py
@@ -1,8 +1,8 @@
-# Flavor ID to use for the host VM
-FLAVOR_ID = '42'
+# Flavor ID to use for the ICaaS agent VM
+FLAVOR_ID = '1'
 
-# Image ID to use for the host VM
-IMAGE_ID = 'b1acb3fe-0496-4510-867c-4cf5f33b196d'
+# Image ID to use for the ICaaS agent VM
+IMAGE_ID = '00000000-0000-0000-0000-00000000000'
 
 # Astakos authentication endpoint
 AUTH_URL = 'https://accounts.okeanos.grnet.gr/identity/v2.0'
@@ -11,7 +11,7 @@ AUTH_URL = 'https://accounts.okeanos.grnet.gr/identity/v2.0'
 COMPUTE_URL = 'https://cyclades.okeanos.grnet.gr/compute/v2.0'
 
 # Set a really long and random key
-SECRET_KEY = 'qqqqqqaaaaaazzzzz'
+SECRET_KEY = '5hh2uo7128pjzv13aqpyx5vvi9j5t75mo6zme88o'
 
 # Cookie name to set for the browser session
 SESSION_COOKIE_NAME = 'icaas_session'
@@ -24,10 +24,10 @@ KAMAKI_SSL_LOCATION = '/etc/ssl/certs/ca-certificates.crt'
 # Debug mode
 DEBUG = True
 
-# ICaaS endpoint URL
-ICAAS_ENDPOINT = "http://host:port/icaas/"
+# ICaaS endpoint URL to be used by the agent VM to update the build status
+ICAAS_ENDPOINT = "http://example.org/icaas/"
 
-# ICaaS Agent config file locaiton
+# ICaaS Agent config file location
 AGENT_CFG = "/etc/icaas/manifest.cfg"
 
 # ICaaS Agent init file
