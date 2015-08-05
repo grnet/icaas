4c64c45e9244902a133185a816230b09beb7ded1
Nikos Skalkotos <skalkoto@grnet.gr>
2015-08-05 17:53:26 +0300

    tests: Add image creation & build update tests

--- icaas/tests.py
+++ icaas/tests.py
@@ -19,28 +19,34 @@
 
 import logging
 
+from flask import json
 from flask.ext.testing import TestCase
 from mock import patch, Mock
 
 import astakosclient
 
 from icaas import create_app, settings
-from icaas.models import db
+from icaas.models import db, Build, User
 
 
 logger = logging.getLogger(__name__)
 
+USER_ID = u'9b8d7062-3768-11e5-854b-aa0cf0567195'
+USER_TOKEN = u'ukzloztfyvrbwffypayzyymtiinlptwzhgeqmoyw'
+VM_ID = '674321'
+
+# Returns an astakosclient response for an successful authorization request
 astakos_authorized = Mock(return_value={
     u'access': {
         u'serviceCatalog': [],
         u'token': {
             u'expires': u'2600-09-13T09:18:29.988089+00:00',
-            u'id': u'test',
+            u'id': USER_TOKEN,
             u'tenant': {
-                u'id': u'9b8d7062-3768-11e5-854b-aa0cf0567195',
+                u'id': USER_ID,
                 u'name': u'Babis Tereres'}},
         u'user': {
-            u'id': u'9b8d7062-3768-11e5-854b-aa0cf0567195',
+            u'id': USER_ID,
             u'name': u'Babis Tereres',
             u'roles': [
                 {u'id': u'5', u'name': u'academic-login-users'},
@@ -48,14 +54,40 @@ astakos_authorized = Mock(return_value={
             u'roles_links': []}}
 })
 
+# Returns an astakosclient response for an unsuccessful authorization request
 astakos_unauthorized = Mock(
     side_effect=astakosclient.Unauthorized(message='UNAUTHORIZED',
                                            details='invalid token'))
+
+# kamaki Mocks
+kamaki_create_server = Mock(return_value={u'id': VM_ID})
+kamaki_delete_server = Mock(return_value="")
+
+
+def create_test_user():
+    """Create a test user to be used on the tests"""
+    user = User(USER_ID)
+    user.token = USER_TOKEN
+    db.session.add(user)
+    db.session.commit()
+    return user
+
+
+def create_test_build():
+    """Create a test build to be used on the tests"""
+    user = create_test_user()
+
+    build = Build(user.id, "Test Image", "http://example.org/image.diskdump",
+                  0, 'images/test.diskdump', 'icaas/log.txt')
+    db.session.add(build)
+    db.session.commit()
+    return (user, build)
 
 
 class IcaasTestCase(TestCase):
     """ICaaS unittests class"""
     def create_app(self):
+        """Create the Flask application"""
         settings.SQLALCHEMY_DATABASE_URI = "sqlite://"
         app = create_app()
         app.config['TESTING'] = True
@@ -63,20 +95,66 @@ class IcaasTestCase(TestCase):
         return app
 
     def setUp(self):
+        """Setup the application's database"""
         db.create_all()
 
     def tearDown(self):
+        """Remove the application's database"""
         db.session.remove()
         db.drop_all()
 
     @patch('astakosclient.AstakosClient.authenticate', astakos_authorized)
     def test_authorized(self):
+        """Test an authorized access request"""
         rv = self.client.get('/icaas', headers=[('X-Auth-Token', 'test')])
         self.assertEquals(rv.status_code, 200)
 
     @patch('astakosclient.AstakosClient.authenticate', astakos_unauthorized)
     def test_unauthorized(self):
+        """Test an unauthorized access request"""
         rv = self.client.get('/icaas', headers=[('X-Auth-Token', 'test')])
         self.assertEquals(rv.status_code, 401)
+
+    @patch('astakosclient.AstakosClient.authenticate', astakos_authorized)
+    @patch('kamaki.clients.cyclades.CycladesComputeClient.create_server',
+           kamaki_create_server)
+    def test_create_image(self):
+        """Test creating a new image"""
+        data = dict(name='PAOK', image='pithos/image.diskdump',
+                    log='pithos/log', url='http://example.org')
+
+        rv = self.client.post('/icaas', headers=[('X-Auth-Token', USER_TOKEN)],
+                              data=json.dumps(data),
+                              content_type='application/json')
+        self.assertEquals(json.loads(rv.data)['id'], 1)
+        builds = Build.query.all()
+        self.assertEquals(len(builds), 1)
+        build = builds[0]
+        self.assertEquals(build.agent, VM_ID)
+        self.assertEquals(build.agent_alive, True)
+        self.assertEquals(build.src, 'http://example.org')
+        self.assertEquals(build.image, 'pithos/image.diskdump')
+        self.assertEquals(build.log, 'pithos/log')
+
+    @patch('astakosclient.AstakosClient.authenticate', astakos_authorized)
+    @patch('kamaki.clients.cyclades.CycladesComputeClient.delete_server',
+           kamaki_delete_server)
+    def test_update_build(self):
+        """Test updating the build status"""
+
+        user, build = create_test_build()
+
+        rv = self.client.put('/icaas/%d' % build.id,
+                             headers=[('X-Icaas-Token', build.token)],
+                             data=json.dumps({'status': 'COMPLETED'}),
+                             content_type='application/json')
+
+        self.assertEquals(rv.status_code, 200)
+
+        rv = self.client.get('/icaas/%d' % build.id,
+                             headers=[('X-AUTH-Token', USER_TOKEN)])
+        self.assertEquals(rv.status_code, 200)
+        data = json.loads(rv.data)
+        self.assertEquals(data['build']['status'], 'COMPLETED')
 
 # vim: set sta sts=4 shiftwidth=4 sw=4 et ai :
